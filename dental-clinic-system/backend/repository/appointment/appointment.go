9399235970545cb2f4a938bbda1d40cde1e43db7
mustafabakirtas <mbakirtas0@outlook.com>
2024-08-18 20:11:10 +0300

    naming fix

--- dental-clinic-system/backend/repository/appointment/appointment.go
+++ dental-clinic-system/backend/repository/appointment/appointment.go
@@ -1,16 +1,16 @@
 package appointment
 
 import (
-	models2 "dental-clinic-system/models"
+	"dental-clinic-system/models"
 	"gorm.io/gorm"
 )
 
 type AppointmentRepository interface {
-	GetAppointments() ([]models2.Appointment, error)
-	GetAppointment(id string) (models2.Appointment, error)
-	CreateAppointment(appointment models2.Appointment) (models2.Appointment, error)
-	UpdateAppointment(appointment models2.Appointment) (models2.Appointment, error)
-	GetAppointmentClinic(id string) (models2.Clinic, error)
+	GetAppointments() ([]models.Appointment, error)
+	GetAppointment(id string) (models.Appointment, error)
+	CreateAppointment(appointment models.Appointment) (models.Appointment, error)
+	UpdateAppointment(appointment models.Appointment) (models.Appointment, error)
+	GetAppointmentClinic(id string) (models.Clinic, error)
 	DeleteAppointment(id string) error
 }
 
@@ -22,48 +22,48 @@ type appointmentRepository struct {
 	DB *gorm.DB
 }
 
-func (r *appointmentRepository) GetAppointments() ([]models2.Appointment, error) {
-	var appointments []models2.Appointment
+func (r *appointmentRepository) GetAppointments() ([]models.Appointment, error) {
+	var appointments []models.Appointment
 	if result := r.DB.Preload("Clinic").Preload("Patient").Preload("Doctor").Find(&appointments); result.Error != nil {
 		return nil, result.Error
 	}
 	return appointments, nil
 }
 
-func (r *appointmentRepository) GetAppointment(id string) (models2.Appointment, error) {
-	var appointment models2.Appointment
+func (r *appointmentRepository) GetAppointment(id string) (models.Appointment, error) {
+	var appointment models.Appointment
 	if result := r.DB.Preload("Clinic").Preload("Patient").Preload("Doctor").First(&appointment, id); result.Error != nil {
-		return models2.Appointment{}, result.Error
+		return models.Appointment{}, result.Error
 	}
 	return appointment, nil
 }
 
-func (r *appointmentRepository) CreateAppointment(appointment models2.Appointment) (models2.Appointment, error) {
+func (r *appointmentRepository) CreateAppointment(appointment models.Appointment) (models.Appointment, error) {
 	if result := r.DB.Create(&appointment); result.Error != nil {
-		return models2.Appointment{}, result.Error
+		return models.Appointment{}, result.Error
 	}
 	return appointment, nil
 }
 
-func (r *appointmentRepository) UpdateAppointment(appointment models2.Appointment) (models2.Appointment, error) {
+func (r *appointmentRepository) UpdateAppointment(appointment models.Appointment) (models.Appointment, error) {
 	if result := r.DB.Save(&appointment); result.Error != nil {
-		return models2.Appointment{}, result.Error
+		return models.Appointment{}, result.Error
 	}
 	return appointment, nil
 }
 
 func (r *appointmentRepository) DeleteAppointment(id string) error {
-	if result := r.DB.Delete(&models2.Appointment{}, id); result.Error != nil {
+	if result := r.DB.Delete(&models.Appointment{}, id); result.Error != nil {
 		return result.Error
 	}
 	return nil
 }
 
-func (r *appointmentRepository) GetAppointmentClinic(id string) (models2.Clinic, error) {
+func (r *appointmentRepository) GetAppointmentClinic(id string) (models.Clinic, error) {
 
-	var clinic models2.Clinic
+	var clinic models.Clinic
 	if result := r.DB.Preload("Appointments").First(&clinic, id); result.Error != nil {
-		return models2.Clinic{}, result.Error
+		return models.Clinic{}, result.Error
 	}
 	return clinic, nil
 }
