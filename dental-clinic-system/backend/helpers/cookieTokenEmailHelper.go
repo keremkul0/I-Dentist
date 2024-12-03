e1822c17b079d0e337b8537be04ecf49dd66ea9a
Kerem Kul <keremkul0@outlook.com>
2024-12-03 20:48:17 +0300

    implement email verification

--- dental-clinic-system/backend/helpers/cookieTokenEmailHelper.go
+++ dental-clinic-system/backend/helpers/cookieTokenEmailHelper.go
@@ -4,18 +4,20 @@ import (
 	"dental-clinic-system/models"
 	"errors"
 	"github.com/golang-jwt/jwt/v5"
+	"net/http"
 )
 
-func TokenEmailHelper(tokenStr string) (*models.Claims, error) {
-	// Token boş mu kontrol et
-	if tokenStr == "" {
-		return nil, errors.New("token is required")
+func CookieTokenEmailHelper(r *http.Request) (*models.Claims, error) {
+	// Extract email from JWT
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		return nil, err
 	}
 
-	// Claims yapısını oluştur
+	tokenStr := cookie.Value
 	claims := &models.Claims{}
 	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
-		return GetJWTKey(), nil // JWT anahtarını sağlayan fonksiyon
+		return GetJWTKey(), nil
 	})
 
 	if err != nil {
